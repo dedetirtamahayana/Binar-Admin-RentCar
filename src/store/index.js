a82b31d5ceec2f46f627408cbfbad521eb82bb11
Dede Mahayana <dedetirtamahayana@gmail.com>
2023-04-08 18:02:25 +0700

    Initial Commit

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import dashboardSlice from "./actions/dashboard-slice";
+import tableSlice from "./actions/table-slice";
+import addSlice from "./actions/add-slice";
+import viewcarSlice from "./actions/view-car-slice";
+import authAdminSlice from "./actions/admin-auth-slice"
+import editSlice from "./actions/edit-slice";
+
+
+
+export const Store = configureStore({
+    reducer:{
+        authAdminStore: authAdminSlice.reducer,
+        dashboardStore : dashboardSlice.reducer,
+        tableStore : tableSlice.reducer,
+        addStore :addSlice.reducer,
+        viewcarStore :viewcarSlice.reducer,
+        editcarStore : editSlice.reducer
+        
+    }
+})
